24f24024965c7601936ba26ed72eb92784a9cde4
kamryb <kamrybinski@gmail.com>
2020-06-18 21:19:15 +0200

    Additional test module

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,8 @@
+package com.kodilla.testing;
+
+public class TestingMain {
+    public static void main(String[] args)
+    {
+        System.out.println("Moudl 6. Wprowadzenie do testowania oprogramowania");
+    }
+}
