71a083e7363d5c59557df5762cf3548392bf22fb
kamryb <kamrybinski@gmail.com>
2020-06-21 23:24:02 +0200

    [6.2]_EasyCalc&test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,20 +1,9 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.user.SimpleUser;
 import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if(result.equals("theForumUser")){
-            System.out.println("test OK");
-        }
-        else{
-            System.out.println("Error!");
-        }
 
         Calculator calc=new Calculator();
 
