d50ffc5b23caa3475bc5f04eb435c1b9360319d5
kamryb <kamrybinski@gmail.com>
2020-08-03 17:41:27 +0200

    6.3 JUnit- final

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,28 +5,25 @@ import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
 
-=======
-        Calculator calc=new Calculator();
+        Calculator calc = new Calculator();
 
-        int resAdd = calc.add(20,4);
-        int resSub = calc.subtract(20,4);
+        int resAdd = calc.add(20, 4);
+        int resSub = calc.subtract(20, 4);
 
-        if(resAdd==24) {
+        if (resAdd == 24) {
             System.out.println("Calc-add working ok");
-        }
-        else {
+        } else {
             System.out.println("Calc-add NO-working");
         }
 
-        if(resSub==16) {
+        if (resSub == 16) {
             System.out.println("Calc-subs working ok");
-        }
-        else {
+        } else {
             System.out.println("Calc-subs NO-working ok");
 
         }
 
     }
+}
 
