af91a1eb6965054e338363f8063e700cf4d9f85d
kamryb <kamrybinski@gmail.com>
2020-09-03 17:34:16 +0200

    7.1v2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,9 @@
 package com.kodilla.stream;
 
-//import com.kodilla.stream.lambda.SaySomething;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
 
-import com.kodilla.stream.reference.FunctionalCalculator;
+
 
 public class StreamMain {
     public static void main(String[] args){
