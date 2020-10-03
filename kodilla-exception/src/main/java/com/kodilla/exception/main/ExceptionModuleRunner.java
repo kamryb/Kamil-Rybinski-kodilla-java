8afeb28a1cd92362e1acc5d59e1a4bc942e7bce9
kamryb <kamrybinski@gmail.com>
2020-10-03 09:30:56 +0200

    8.1v1'

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,4 +1,20 @@
 package com.kodilla.exception.main;
 
+import  com.kodilla.exception.io.FileReader;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class ExceptionModuleRunner {
+
+    public static void main(String args[]) {
+
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+
+    }
+
+
 }
