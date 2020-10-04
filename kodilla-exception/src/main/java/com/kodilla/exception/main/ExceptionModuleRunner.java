b9462f7a581503546b0e4d46bee7b2ebfa41e1e8
kamryb <kamrybinski@gmail.com>
2020-10-04 23:24:49 +0200

    8.3v1

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,7 +1,11 @@
 package com.kodilla.exception.main;
 
 import  com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWthoutHandling;
+import com.kodilla.exception.test.SecondChallenge;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,10 +13,14 @@ import java.util.stream.Stream;
 
 public class ExceptionModuleRunner {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
 
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
+      SecondChallenge secondChallenge = new SecondChallenge();
+      try {
+          secondChallenge.probablyIWillThrowException(2,1.5);
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
 
     }
 
