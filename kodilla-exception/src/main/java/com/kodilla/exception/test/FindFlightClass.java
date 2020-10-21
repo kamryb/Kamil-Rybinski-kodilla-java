d31b848332cff2f70911307b7ed40266625bc01d
kamryb <kamrybinski@gmail.com>
2020-10-21 21:57:47 +0200

    8.4_v1

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlightClass.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlightClass.java
@@ -1,4 +1,30 @@
 package com.kodilla.exception.test;
+//import sun.font.TrueTypeFont;
+
+import java.util.*;
 
 public class FindFlightClass {
+
+    Map<String, Boolean> airport = new HashMap<>();
+
+    public FindFlightClass(){
+        airport.put("Wawa", true);
+        airport.put("Kat", true);
+        airport.put("Olsztyn", false);
+    }
+    
+    public boolean findFlight(Flight flight){
+
+        if(airport.containsKey(flight.arrivalAirport)) {
+            return airport.get(flight.arrivalAirport);
+        }
+        throw new RouteNotFoundException("No Airport");
+    }
+
+    public static void main(String[] args){
+        FindFlightClass findFlight=new FindFlightClass();
+        Flight flight1 =new Flight("Wawa","Kat");
+        findFlight.findFlight(flight1);
+
+    }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -1,4 +1,8 @@
 package com.kodilla.exception.test;
 
-public class RouteNotFoundException {
+public class RouteNotFoundException extends RuntimeException{
+    public RouteNotFoundException(final String message) {
+        super(message);
+    }
+
 }
