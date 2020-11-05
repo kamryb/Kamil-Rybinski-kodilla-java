8649df7b2f28523473b85041ffd67f2d93b6e63b
kamryb <kamrybinski@gmail.com>
2020-11-05 19:55:24 +0100

    10.4v1

---
+++ kodilla-spring-wwweb/src/main/java/com/kodilla/kodillaspringwwweb/KodillaSpringWwwebApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.kodillaspringwwweb;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class KodillaSpringWwwebApplication {
+
+	public static void main(String[] args) {
+		run(KodillaSpringWwwebApplication.class, args);
+	}
+
+}
---
+++ kodilla-spring-wwweb/src/test/java/com/kodilla/kodillaspringwwweb/KodillaSpringWwwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillaspringwwweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringWwwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
